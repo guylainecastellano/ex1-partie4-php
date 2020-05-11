1609a77c51cf57e89ca8646775cf7e3d343a7d53
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-11 14:42:25 +0200

    ex1 partie4 php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-$phrase = 'chaine de caractère';
+//on déclare une fonction
+function text ($phrase) {
+//on retourne le paramètre
+return $phrase;
+}
+//on passe un paramètre text à la fonction
+text ('chaine de caractère');
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -9,7 +15,11 @@ $phrase = 'chaine de caractère';
 </head>
 <body>
   <?php
-  echo 'chaine de caractère ' .$phrase;
-  ?>
+  //si on affiche le paramètre
+  echo text('chaine de caractère');
+//echo text
+//echo de la variable
+  $word = text ('ceci est une chaine de caractères');?>
+  <p><?php echo $word;?></p>
 </body>
 </html>

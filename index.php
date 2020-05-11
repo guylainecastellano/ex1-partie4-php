db1a1d7d302a88ff9446f250a623cdf0f1fa6d7a
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-11 11:40:03 +0200

    ex1 partie4 php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$phrase = 'chaine de caractère';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex1 partie4 php</title>
+</head>
+<body>
+  <?php
+  echo 'chaine de caractère ' .$phrase;
+  ?>
+</body>
+</html>
